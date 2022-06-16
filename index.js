e1e63f542f7a37bd877f32e6b8297b406f5ba884
Edwin George <edwgeo@gmail.com>
2022-06-16 00:31:09 -0700

    pass data to frontend as JSON

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ const app = express()
 
 app.use(express.static('public'))
 
-app.get('/', async (req, res) => {
+app.get('/api', async (req, res) => {
     let data = await fs.readFile('./books.json', 'utf8')
     data = JSON.parse(data);
-    res.setHeader("Content-Type", "text/html")
-    data.forEach(element => {
-        res.write("<div><img src='"+element.imageLink+"'/></div>")
-    });
-    res.send()
+    res.json(data)
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'))
